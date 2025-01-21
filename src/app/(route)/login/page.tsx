419ed1e9ddfada1acb8ebd1d8803f83953ec0a2d
seunghyun <tmdgus7820@naver.com>
2025-01-21 22:58:49 +0900

    🛠️refactor: 로그인 페이지 내부 로직을 재사용 가능한 컴포넌트로 분리

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,39 +1,12 @@
-import styles from "./login.module.css";
-import LoginForm from "./_components/LoginForm/LoginForm";
-import Image from "next/image";
+import { MainBanner, SpaceImage } from "@/app/_components/server";
+import { LoginForm } from "./_components";
 
 const LoginPage = () => {
   return (
-    <section className={styles.loginSection}>
-      <article className={styles.titleContainer}>
-        <h2>WORLD MISS UNIVERSITY</h2>
-        <h3>CAMBODIA 2024</h3>
-      </article>
-      <div className={styles.imgBox}>
-        <Image
-          className={styles.tiaraImg}
-          src="/images/tiara_crown.png"
-          alt="world miss university cambodia 2024 tiara crown"
-          fill
-          priority
-        />
-      </div>
-      <article className={styles.contentContainer}>
-        <span>Cast your vote for the brightest cadidate!</span>
-        <span>World Miss University voting starts now!</span>
-      </article>
+    <MainBanner>
       <LoginForm />
-
-      <div className={styles.imgBox}>
-        <Image
-          className={styles.spaceImg}
-          src="/images/space.png"
-          alt="space"
-          fill
-          priority
-        />
-      </div>
-    </section>
+      <SpaceImage />
+    </MainBanner>
   );
 };
 
