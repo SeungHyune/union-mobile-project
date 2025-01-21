c3f67a01a4de3fc34a37b23c4eaffff4327fc023
seunghyun <tmdgus7820@naver.com>
2025-01-21 22:52:51 +0900

    💎feat: 로그인 페이지 포스터 이미지 및 문구 MainBanner 컴포넌트로 분리 - 로그인 & 메인 페이지에서 재사용 가능 컴포넌트

---
+++ src/app/_components/server/MainBanner/MainBanner.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./mainBanner.module.css";
+
+interface MainBannerProps {
+  children: React.ReactNode;
+}
+
+const MainBanner = ({ children }: MainBannerProps) => {
+  return (
+    <section className={styles.mainBannerSection}>
+      <article className={styles.titleContainer}>
+        <h2>WORLD MISS UNIVERSITY</h2>
+        <h3>CAMBODIA 2024</h3>
+      </article>
+      <div className={styles.imgBox}>
+        <Image
+          className={styles.tiaraImg}
+          src="/images/tiara_crown.png"
+          alt="world miss university cambodia 2024 tiara crown"
+          fill
+          priority
+        />
+      </div>
+      <article className={styles.contentContainer}>
+        <span>Cast your vote for the brightest cadidate!</span>
+        <span>World Miss University voting starts now!</span>
+      </article>
+      {children}
+    </section>
+  );
+};
+
+export default MainBanner;
