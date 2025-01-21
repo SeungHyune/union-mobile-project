15d9ef7a59af554010ce6e1600b24a25a7e3cd82
seunghyun <tmdgus7820@naver.com>
2025-01-21 18:38:56 +0900

    🎨style: 로그인 페이지 UI 제작 - 레이아웃 및 디자인 완성 - poster 이미지를 넣으면 반응형에 좋지 않은 UI 경험을 줄 것 같아, Title, Image를 직접 퍼블리싱

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,5 +1,40 @@
+import styles from "./login.module.css";
+import LoginForm from "./_components/LoginForm/LoginForm";
+import Image from "next/image";
+
 const LoginPage = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <section className={styles.loginSection}>
+      <article className={styles.titleContainer}>
+        <h2>WORLD MISS UNIVERSITY</h2>
+        <h3>CAMBODIA 2024</h3>
+      </article>
+      <div className={styles.imgBox}>
+        <Image
+          className={styles.tiaraImg}
+          src="/images/tiara_crown.png"
+          alt="world miss university cambodia 2024 tiara crown"
+          fill
+          priority
+        />
+      </div>
+      <article className={styles.contentContainer}>
+        <span>Cast your vote for the brightest cadidate!</span>
+        <span>World Miss University voting starts now!</span>
+      </article>
+      <LoginForm />
+
+      <div className={styles.imgBox}>
+        <Image
+          className={styles.spaceImg}
+          src="/images/space.png"
+          alt="space"
+          fill
+          priority
+        />
+      </div>
+    </section>
+  );
 };
 
 export default LoginPage;
