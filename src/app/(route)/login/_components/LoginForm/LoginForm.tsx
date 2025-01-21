0bdaa2ad7118ae6885f57980a0f28bf6cb5bed72
seunghyun <tmdgus7820@naver.com>
2025-01-21 18:37:01 +0900

    💎feat: Login 페이지 LoginForm 컴포넌트 제작 - 아이디를 입력하여 로그인하면 localStorage에 로그인한 아이디가 저장됩니다. - Header의 오른쪽 X(CloseIcon)을 통해 로그아웃할 수 있습니다. - Login이 완료되었다면 /login 페이지에 접근할 수 없습니다. (/login 페이지 접근 시 자동으로 / 페이지로 리다이렉트 됩니다.) - LoginId는 영문, 숫자 조합으로 3자리 이상 입력해야 합니다. (validate에 통과하지 못하는 경우 로그인이 되지 않으며 통과하지 못한 Message를 안내합니다.) - 정상적으로 로그인에 성공했다면, / 페이지로 리다이렉트 됩니다.

---
+++ src/app/(route)/login/_components/LoginForm/LoginForm.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const LoginForm = () => {
+  const [loginId, setLoginId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [isLoginValidate, setIsLoginValidate] = useState(true);
+  const [loginErrorMessage, setLoginErrorMessage] = useState("");
+
+  useEffect(() => {
+    const storagedLoginId = localStorage.getItem("loginId") || "";
+    setLoginId(storagedLoginId);
+
+    if (loginId === "") {
+      return;
+    }
+
+    redirect("/");
+  }, [loginId]);
+
+  const handleLoginIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(event.target.value);
+
+    if (isLoginValidate === false) {
+      setIsLoginValidate(false);
+      setLoginErrorMessage("");
+    }
+  };
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const loginId = userId.trim();
+    if (loginId === "" || loginId.length < 3) {
+      setIsLoginValidate(false);
+      setLoginErrorMessage("The ID must be at least 3 characters long.");
+      return;
+    }
+
+    const loginValidate = /^[a-zA-Z0-9]+$/;
+    if (!loginValidate.test(loginId)) {
+      setIsLoginValidate(false);
+      setLoginErrorMessage("The ID can only contain letters and numbers.");
+      return;
+    }
+
+    localStorage.setItem("loginId", userId);
+    setLoginId(userId);
+  };
+
+  return (
+    <form onSubmit={handleLoginSubmit}>
+      <div>
+        <input
+          type="text"
+          name="userId"
+          id="userId"
+          placeholder="Enter your ID"
+          value={userId}
+          onChange={handleLoginIdChange}
+        />
+        {isLoginValidate === false ? (
+          <p>
+            <span>{loginErrorMessage}</span>
+          </p>
+        ) : null}
+      </div>
+      <button>Log in</button>
+    </form>
+  );
+};
+
+export default LoginForm;
