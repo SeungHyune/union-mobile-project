c756fb5065b3471169db7a4a0d5c6c84894943dd
seunghyun <tmdgus7820@naver.com>
2025-01-22 16:08:06 +0900

    🐛fix: 허용된 원격 이미지 호스트 추가 - 외부 이미지 URL을 사용하기 위해 next.config.ts 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "angkorchat-bucket.s3.ap-southeast-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
