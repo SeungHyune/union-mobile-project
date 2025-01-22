7282ff1da02a328018e3c99d1df8395476ce30df
seunghyun <tmdgus7820@naver.com>
2025-01-22 15:34:55 +0900

    📝docs: candidate api endpoint를 _constants 파일로 작성 - 유지 보수성 및 재사용성을 위해 상수로 관리

---
+++ src/app/_constants/endPoint/candidate/candidate.ts
@@ -0,0 +1,7 @@
+const CANDIDATE = {
+  LIST: "/vote/candidate/list",
+  DETAIL: "/vote/candidate",
+  VOTED_LIST: "/vote/voted/candidate/list",
+};
+
+export default CANDIDATE;
---
+++ src/app/_constants/endPoint/index.ts
@@ -0,0 +1,1 @@
+export { default as CANDIDATE } from "./candidate/candidate";
