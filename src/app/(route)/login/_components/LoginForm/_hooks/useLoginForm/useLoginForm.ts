475707cb26f665f00ffc9e02445e1e89d8c9a513
seunghyun <tmdgus7820@naver.com>
2025-01-23 13:41:25 +0900

    🛠️refactor: Login 페이지 리팩토링 - LoginForm 컴포넌트 useLoginForm hook으로 기능 분리 - 로그인 유효성 검사(아이디는 3 글자 이상~16 글자 이하, 영문 숫자만 가능합니다.) 및 로그인 성공 시 로그인 아이디를 localStorage에 저장하여 로그인 상태 유지

---
+++ src/app/(route)/login/_components/LoginForm/_hooks/useLoginForm/useLoginForm.ts
@@ -0,0 +1,59 @@
+import { useLoginInfoStore } from "@/app/_store/useLoginInfoStore/useLoginInfoStore";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const useLoginForm = () => {
+  const { loginId, setLoginId } = useLoginInfoStore();
+  const [userId, setUserId] = useState("");
+  const [isLoginValidate, setIsLoginValidate] = useState(true);
+  const [loginErrorMessage, setLoginErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (loginId === "") {
+      return;
+    }
+
+    redirect("/");
+  }, [loginId]);
+
+  const handleLoginIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(event.target.value);
+
+    if (isLoginValidate === false) {
+      setIsLoginValidate(false);
+      setLoginErrorMessage("");
+    }
+  };
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const loginId = userId.trim();
+    if (loginId === "" || loginId.length < 3 || loginId.length > 16) {
+      setIsLoginValidate(false);
+      setLoginErrorMessage(
+        "Username must be between 3 and 16 characters long.",
+      );
+      return;
+    }
+
+    const loginValidate = /^[a-zA-Z0-9]+$/;
+    if (!loginValidate.test(loginId)) {
+      setIsLoginValidate(false);
+      setLoginErrorMessage("The ID can only contain letters and numbers.");
+      return;
+    }
+
+    setLoginId(userId);
+  };
+
+  return {
+    userId,
+    isLoginValidate,
+    loginErrorMessage,
+    handleLoginIdChange,
+    handleLoginSubmit,
+  };
+};
+
+export default useLoginForm;
